36df2927fda518b557f1eb5eb73e651f78301cb5
Varun2024 <marvellous079@gmail.com>
2026-01-18 00:46:40 +0530

    home complete

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
 
-const CustomButton = () => {
+import state from '../store'
+import { useSnapshot } from 'valtio';
+const CustomButton = ({ type, title, handleClick, customStyles }) => {
+
+    const snap = useSnapshot(state);
+    const generateStyle = (type) => {
+        if (type === 'filled') {
+            return {
+                backgroundColor: snap.color,
+                color: '#FFFFFF',
+            }
+        }
+    }
   return (
-    <div>CustomButton</div>
+    <button
+        className={`px-2 py-1.2 flex-1 rounded-md font-semibold ${customStyles} `}
+        style={generateStyle(type)}
+        onClick={handleClick}
+    >
+        {title}
+    </button>
   )
 }
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
+import {useSnapshot} from 'valtio'
+import state from '../store'
+import { AnimatePresence,motion } from 'framer-motion'
+
+import config from '../config/config'
+import { download } from '../assets'
+import { downloadCanvasToImage,reader } from '../config/helpers'
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants'
+import { fadeAnimation, slideAnimation } from '../config/motion'
+// import { AIPicker, ColorPicker, FilePicker, Tab } from '../components'
+
 
 const Customizer = () => {
   return (
